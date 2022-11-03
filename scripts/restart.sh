1b350fb620915c7f7435a7392b5fcac540bd075d
Pavel Lots <pavel418890@gmail.com>
2022-11-04 00:11:30 +0300

    update backend imports

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -9,5 +9,6 @@ DOCKER=/usr/bin/docker
 find $PROJECT_PATH -type f -name "*.pyc" -o -name "*.pyo" -exec rm -fr "{}" \;
 
 $DOCKER compose down
+$DOCKER compose build --build-arg DOMAIN_ARG=$DOMAIN --build-arg PROJECT_PATH_ARG=$PROJECT_PATH
 $DOCKER compose	-f docker-compose.yaml up -d --force-recreate pg rabbit redis nginx backend celeryworker 
 
